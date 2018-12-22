6ee07ca18c5dc1f5a88036f46cc30a944d2b8e81
leehmaciel <lcbm2@cin.ufpe.br>
2018-12-22 07:44:59 -0300

    Updates

---
+++ precache-manifest.652a47dd3bc9a0e3a7c9008ffd674232.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b4f1bf95c4d0c3b794f71a803a0338d",
+    "url": "/static/media/twitter.9b4f1bf9.svg"
+  },
+  {
+    "revision": "e07fcd67d4936e990dad07b397c5f1ab",
+    "url": "/static/media/linkedin.e07fcd67.svg"
+  },
+  {
+    "revision": "cde0c062f2642e8e4253d0455d93592e",
+    "url": "/static/media/github.cde0c062.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "254e071ea99ebf7aab5b",
+    "url": "/static/js/main.254e071e.chunk.js"
+  },
+  {
+    "revision": "674cb3e287552b84cb1b",
+    "url": "/static/js/1.674cb3e2.chunk.js"
+  },
+  {
+    "revision": "254e071ea99ebf7aab5b",
+    "url": "/static/css/main.18314995.chunk.css"
+  },
+  {
+    "revision": "0f274752e052592eb4cabd426b6d0a38",
+    "url": "/index.html"
+  }
+];
